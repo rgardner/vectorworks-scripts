caeaf09987036645c1eaa81ce7437892939c45db
Bob Gardner <bob.hn.gardner@gmail.com>
2020-12-29 12:17:58 -0800

    Log all

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -10,6 +10,7 @@ def main():
     output.append("lines: {}".format(vs.Count("T=LINE")))
     output.append("rects: {}".format(vs.Count("T=RECT")))
     output.append("verts: {}".format(vs.Count("T=VERT")))
+    output.append("all: {}".format(vs.Count("ALL")))
     vs.AlrtDialog("\n".join(output))
 
 
