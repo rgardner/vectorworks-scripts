39f22ce4bd453de0cb7ec9fa157c7a37c0db63cf
Bob Gardner <bob.hn.gardner@gmail.com>
2020-12-29 09:52:39 -0800

    Add pre-commit setup and ci to x

--- x.py
+++ x.py
@@ -5,6 +5,7 @@
 import argparse
 import io
 import pathlib
+import subprocess
 import sys
 import urllib.request
 import zipfile
@@ -14,7 +15,14 @@ VS_ZIP_FILE_URL = "https://developer.vectorworks.net/images/c/c5/Vs.zip"
 SCRIPT_DIR = pathlib.Path(__file__).absolute().parent
 
 
-def setup() -> None:
+def run_setup() -> None:
+    subprocess.run(
+        ["pip", "install", "--requirement", str(SCRIPT_DIR / "requirements.txt")],
+        check=True,
+    )
+
+    subprocess.run(["pre-commit", "install"], check=True)
+
     output = SCRIPT_DIR / "build"
     output.mkdir(exist_ok=True)
     if not (output / "vs.py").exists():
@@ -23,14 +31,29 @@ def setup() -> None:
                 zipf.extract("vs.py", output)
 
 
+def run_ci() -> None:
+    subprocess.run(["pre-commit", "run", "--all-files"], check=True)
+
+
 def parse_args(args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
+    subparsers = parser.add_subparsers()
+
+    setup_parser = subparsers.add_parser("setup")
+    setup_parser.set_defaults(func=run_setup)
+
+    ci_parser = subparsers.add_parser("ci")
+    ci_parser.set_defaults(func=run_ci)
+
     return parser.parse_args(args)
 
 
 def main(raw_args: List[str]) -> None:
-    parse_args(raw_args)
-    setup()
+    args = parse_args(raw_args)
+    if (func := getattr(args, "func", None)) is not None:
+        func()  # pylint: disable=not-callable
+    else:
+        run_setup()
 
 
 if __name__ == "__main__":
