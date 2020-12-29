8268809704beb3dc242a39f0ad2121d86b8eb22b
Bob Gardner <bob.hn.gardner@gmail.com>
2020-12-28 20:06:30 -0800

    Count lines and vertices

--- analyze.py
+++ analyze.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
+"""Analyze VectorWorks file for notable signatures."""
+
 import vs
 
 
 def main():
-    num = 0
-    def count(_obj):
-        nonlocal num
-        num += 1
-    vs.ForEachObject(count, "T=LINE")
-    vs.AlrtDialog(f"num lines: {num}")
+    output = []
+    output.append("lines: {}".format(vs.Count("T=LINE")))
+    output.append("rects: {}".format(vs.Count("T=RECT")))
+    output.append("verts: {}".format(vs.Count("T=VERT")))
+    vs.AlrtDialog("\n".join(output))
 
 
 if __name__ == "__main__":
