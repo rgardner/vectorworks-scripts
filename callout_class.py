f3f17347987f4fb2872cae2d3ce6a1b71c2bea46
Bob Gardner <bob.hn.gardner@gmail.com>
2020-12-29 09:30:07 -0800

    Add logging to callout class

--- callout_class.py
+++ callout_class.py
@@ -2,28 +2,40 @@
 
 """Assigns all elements of a class to a given class."""
 
+import logging
+import pathlib
+
 import vs  # pylint: disable=syntax-error
 
 CLASS = "ANNO-NOTES"
 IGNORE_CLASS = "ANNO-GRAPHIC TAGS"
+LOG_LEVEL = logging.DEBUG
+
+# https://developer.vectorworks.net/index.php/VS:GetFolderPath
+USER_PLUGINS_DIR = -2
+
+
+def setup_logging() -> None:
+    filename = pathlib.Path(vs.GetFolderPath(USER_PLUGINS_DIR)) / "adi_script.log"
+    logging.basicConfig(filename=filename, level=LOG_LEVEL)
 
 
 def main() -> None:
+    setup_logging()
     updated = 0
     total = 0
 
     def set_class(obj) -> None:
         nonlocal updated
         nonlocal total
-        curr_class = vs.GetClass(obj)
-        if curr_class == IGNORE_CLASS:
-            return
-        if curr_class != CLASS:
+        klass = vs.GetClass(obj)
+        logging.debug("%s class: %s", vs.GetName(obj), klass)
+        if klass != CLASS:
             vs.SetClass(obj, CLASS)
             updated += 1
         total += 1
 
-    vs.ForEachObject(set_class, "T=CALLOUT")
+    vs.ForEachObject(set_class, "(R IN ['Callout']) & C<>'{}'".format(IGNORE_CLASS))
     vs.AlrtDialog("Updated {} of {} callouts".format(updated, total))
 
 
